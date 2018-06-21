be7247ac4bd838cb56316211abbb5f9cba89a5f0
Jose Leon <jleon43@bloomberg.net>
2018-06-21 06:52:28 -0400

    wip

--- src/components/Branch.jsx
+++ src/components/Branch.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Branch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true
-        };
-        // name, children[], active, collapsed, icon, level, canEdit, canDelete
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+    // name, children[], active, collapsed, icon, level, canEdit, canDelete
+  }
 
-    render() {
-        return (
-            <div>
-                <div>{this.props.data.name}</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div>Branch: {this.props.data.name}</div>
+      </div>
+    );
+  }
 }
 
 Branch.propTypes = {
-    data: PropTypes.object.isRequired
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Branch;
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,38 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Branch from './Branch';
 
 class Tree extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true
-        };
-        // name, children[], active, collapsed, icon, canEdit, canDelete
-        // onChange
-        // linkable ZK paths /engines/solr/foo
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+    // name, children[], active, collapsed, icon, canEdit, canDelete
+    // onChange
+    // linkable ZK paths /engines/solr/foo
+  }
 
-    render() {
-        return (
-            <div>
-                <div>{this.props.contents.name}</div>
-                <div>{this.props.contents.foo}</div>
-                <div>Toggled: {this.props.contents.toggled.toString()}</div>
-                <div>Active: {this.props.contents.active.toString()}</div>
-                {this.props.contents.children.map(function(child, i){
-                    return <Branch data={child}/>
-                )}
-            </div>
-        );
-    }
+  foo() {
+    // var i, result = [];
+    // for (i = 0; i < children.length; i++) {
+    //   console.log("Creating Branch:" + children[i].name);
+    //   result.push(<Branch data={children[i]}/>);
+    // }
+    // return result;
+    const children = this.props.contents.children.map((c) =>
+      <Branch key={c.name} data={c}/>
+    );
+    return children;
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{this.props.contents.name}</div>
+        <div>{this.props.contents.foo}</div>
+        <div>Toggled: {this.props.contents.toggled.toString()}</div>
+        <div>Active: {this.props.contents.active.toString()}</div>
+        Foobar: {this.foo()}
+      </div>
+    );
+  }
 }
 
 Tree.propTypes = {
-    contents: PropTypes.shape({
-        name: PropTypes.string.isRequired
-    }).isRequired
+  contents: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Tree;
